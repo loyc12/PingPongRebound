2186f0ddfc85095885e518394c1496c716bcae83
Loïc Lord <lordloyc@gmail.com>
2023-11-08 17:22:33 -0500

    minor (gitquick)

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ import Pongest
 
 # MASTER LIST
 # TODO : make a class to handle the game logic
-# TODO : make an 'interface' class to reroute the key events to, for future web use
+# TODO : finish 'interface' class to reroute the key events to
+# TODO : try with websockets (?)
 # TODO : make an ai class to control the rackets throught the interface
 # TODO : split logic and rendering
 # TODO : allow racket control through sockets
 
 # MINOR LIST
-# TODO : use rect center for positioning (in GameObject class)
 # TODO : make an array of balls and rackets instead, so that the number can vary during runtime
-# TODO : make the ball restart's trajectory more random
+# TODO : make a simpler 'ping' game (solo)
+# TODO : make an 'asteroids' game (solo)
+ # TODO : make the ball restart's trajectory more random
 # TODO : add sound effects to collisions (in GameObject class)
 
 def main():
